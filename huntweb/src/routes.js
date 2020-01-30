eab4bd7a13ae6b141feaf3dbdce4a7e8e72367be
Paulo Bruno <paulo.bf111@gmail.com>
2020-01-30 01:12:43 -0300

    Route Edit Product Added

--- huntweb/src/routes.js
+++ huntweb/src/routes.js
@@ -2,14 +2,15 @@ import React from 'react';
 
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Main from './pages/main';
-import Product from './pages/product';
-
+import Product from './pages/procuct_page/product';
+import Edit_Product from './pages/procuct_page/product_edit';
 
 const Routes = () => (
     <BrowserRouter>
         <Switch>
             <Route exact path="/" component={Main} />
-            <Route path="/products/:id" component={Product} />
+            <Route path="/product/:id" component={Product} />
+            <Route path="/product_edit/:id" component={Edit_Product} />
         </Switch>
     </BrowserRouter>
 );
